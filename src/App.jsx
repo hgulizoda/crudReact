6830db5ca404db1b537be159101a7130c8ab6a5f
hgulizoda <hgulizodait@gmail.com>
2025-10-30 19:01:46 +0500

    pagination done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,53 @@
+import { useEffect, useState } from "react";
 import RecipeReviewCard from "./components/Card";
+import loadingIcon from "./assets/loading (2).png";
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(0);
+  const limit = 12;
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(`https://dummyjson.com/recipes?skip=${page * limit}&limit=${limit}`)
+      .then((res) => res.json())
+      .then((res) => {
+        setProducts(res.recipes);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [page, limit]);
+
   return (
-    <div>
-      <RecipeReviewCard />
-    </div>
+    <>
+      <h1>Recipes</h1>
+      <div className="card-holder container">
+        {isLoading ? (
+          <div className="loading">
+            <img src={loadingIcon} alt="" />
+            <p>Loading...</p>
+          </div>
+        ) : (
+          products.map((product) => {
+            console.log(product.name);
+            return <RecipeReviewCard key={product.id} {...product} />;
+          })
+        )}
+      </div>
+      <div className="buttons">
+        <button disabled={page === 0} onClick={() => page && setPage(page - 1)}>
+          Prev
+        </button>
+        <button
+          disabled={page >= Math.ceil(100 / limit) - 1}
+          onClick={() => setPage(page + 1)}
+        >
+          Next
+        </button>
+      </div>
+    </>
   );
 };
 
---
+++ src/components/Pagination.jsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import Pagination from "@mui/material/Pagination";
+import PaginationItem from "@mui/material/PaginationItem";
+import Stack from "@mui/material/Stack";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+
+export default function CustomIcons({ page, count, onChange }) {
+  return (
+    <Stack spacing={2}>
+      <Pagination
+        page={page}
+        count={count}
+        onChange={onChange}
+        renderItem={(item) => (
+          <PaginationItem
+            slots={{ previous: ArrowBackIcon, next: ArrowForwardIcon }}
+            {...item}
+          />
+        )}
+      />
+    </Stack>
+  );
+}
